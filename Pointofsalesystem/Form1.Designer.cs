f7ca843072247042154caa243f5a7ed5afa876b5
cjaestrante-max <cjaestrante@pcu.edu.ph>
2026-03-18 00:41:38 +0800

    first commit

---
+++ Pointofsalesystem/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.lblUsername = new System.Windows.Forms.Label();
+			this.lblPassword = new System.Windows.Forms.Label();
+			this.tbUsername = new System.Windows.Forms.TextBox();
+			this.tbPassword = new System.Windows.Forms.TextBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitle.Location = new System.Drawing.Point(206, 45);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(285, 31);
+			this.lblTitle.TabIndex = 0;
+			this.lblTitle.Text = "Point of Sale System";
+			// 
+			// lblUsername
+			// 
+			this.lblUsername.AutoSize = true;
+			this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblUsername.Location = new System.Drawing.Point(78, 141);
+			this.lblUsername.Name = "lblUsername";
+			this.lblUsername.Size = new System.Drawing.Size(130, 29);
+			this.lblUsername.TabIndex = 1;
+			this.lblUsername.Text = "Username;";
+			// 
+			// lblPassword
+			// 
+			this.lblPassword.AutoSize = true;
+			this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPassword.Location = new System.Drawing.Point(78, 206);
+			this.lblPassword.Name = "lblPassword";
+			this.lblPassword.Size = new System.Drawing.Size(126, 29);
+			this.lblPassword.TabIndex = 2;
+			this.lblPassword.Text = "Password;";
+			// 
+			// tbUsername
+			// 
+			this.tbUsername.Location = new System.Drawing.Point(247, 150);
+			this.tbUsername.Name = "tbUsername";
+			this.tbUsername.Size = new System.Drawing.Size(252, 20);
+			this.tbUsername.TabIndex = 3;
+			// 
+			// tbPassword
+			// 
+			this.tbPassword.Location = new System.Drawing.Point(247, 215);
+			this.tbPassword.Name = "tbPassword";
+			this.tbPassword.Size = new System.Drawing.Size(252, 20);
+			this.tbPassword.TabIndex = 4;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(457, 291);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(75, 23);
+			this.btnLogin.TabIndex = 5;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			// 
+			// frmHome
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(645, 363);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.tbPassword);
+			this.Controls.Add(this.tbUsername);
+			this.Controls.Add(this.lblPassword);
+			this.Controls.Add(this.lblUsername);
+			this.Controls.Add(this.lblTitle);
+			this.Name = "frmHome";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Pointofsalesystem/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+using System.Data;
+
+using System.Drawing;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp1
+
+{
+
+    public partial class frmHome : Form
+
+    {
+
+        public frmHome()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+
+
+
+        }
+
+
+
+        private void label3_Click(object sender, EventArgs e)
+
+        {
+
+
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+
+        {
+
+
+
+        }
+
+        string[,] userCredentials =
+
+        {
+
+            {"admin","1234","Chris Joseph Estrante" },
+
+            {"cashier","1234","Kevin Durant" }
+
+        };
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+            if (tbUsername.Text == "")
+
+            {
+
+                MessageBox.Show("Please enter username!", "validation");
+
+                tbUsername.Focus();
+
+            }
+
+            else if (tbPassword.Text == "")
+
+            {
+
+                MessageBox.Show("Please enter password!", "validation");
+
+                tbPassword.Focus();
+
+            }
+
+            else
+
+            {
+
+                for (int x = 0; x < userCredentials.GetLength(0); x++)
+
+                {
+
+                    if (tbUsername.Text == userCredentials[x, 0])
+
+                    {
+
+                        if (tbPassword.Text == userCredentials[x, 1])
+
+                        {
+
+                            frmHome frm = new frmHome();
+
+                            MessageBox.Show("Welcome " + userCredentials[x, 2]);
+
+                            this.Hide();
+
+                            frm.Show();
+
+                            break;
+
+                        }
+
+                        else
+
+                        {
+
+                            MessageBox.Show("Invalid Username/Password");
+
+                            break;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
